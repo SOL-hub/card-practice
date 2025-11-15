a684f80d4a7e270ceac4489214498e01ad96b06c
parksol <thfdl0317@naver.com>
2025-11-16 03:14:02 +0900

    chore: Emotion 적용 및 타입스크립트 설정 수정

--- react_test_project_card/src/App.tsx
+++ react_test_project_card/src/App.tsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { css } from '@emotion/react'
+
+const containerStyle = css`
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: red;
+`
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div css={containerStyle}>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +22,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 css={containerStyle}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
